544d3776f33c0b0da771a2f1c2ae1305f28e26e1
outjam <flowerave@proton.me>
2023-01-30 18:23:26 +0300

    Add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import Quiz from "react-quiz-component";
 import './App.css';
+
+export const quiz = {
+  quizTitle: "Flowerave",
+  quizSynopsis: "Тествовый опрос",
+  questions: [
+    {
+      // Вопрос
+      question: "Flowerave это?",
+      // Тип
+      questionType: "text",
+      answerSelectionType: "single",
+      // Ответы
+      answers: ["Заебись", "Хуево"],
+      // Вывод - Верный ответ
+      correctAnswer: "1",
+      messageForCorrectAnswer: "Ты верно думаешь!",
+      // Вывод - Неверный ответ
+      messageForIncorrectAnswer: "Неверный ответ",
+      // Вывод отпроса
+      explanation: "Вывод",
+      point: "1"
+    },
+
+    // Вопрос 2
+    {
+      // Вопрос
+      question: "Ты уже купил NFT?",
+      // Тип
+      questionType: "text",
+      answerSelectionType: "single",
+      // Ответы
+      answers: ["Да", "Еще нет"],
+      // Вывод - Верный ответ
+      correctAnswer: "1",
+      messageForCorrectAnswer: "Займи до зп, плиз",
+      // Вывод - Неверный ответ
+      messageForIncorrectAnswer: "А чего ждем?",
+      // Вывод отпроса
+      explanation: "Вывод",
+      point: "1"
+    }
+  ]
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Quiz quiz={quiz} shuffle={true} showInstantFeedback={true} />
     </div>
   );
 }
